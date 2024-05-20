8cdaaec72858330cbb9cc5acc13ac7e1f528ca0f
Juan <arzzdeveloper04@gmail.com>
2024-05-20 04:29:48 -0600

    uipdate

--- js/script.js
+++ js/script.js
@@ -15,91 +15,50 @@ function toggleSucursal() {
   };
 }
 
-function sectionButtons(sucursal) {
-  let section = "";
+function generateButtonSection(sucursal, baseUrl) {
+  return `
+    <section class="section-container">
+      <h1>PANTALLAS</h1>
+      <ul class="container-boxes">
+        <li class="button-box"> 
+          <a href="${baseUrl}/res.html">
+            <img src="./public/images/res.png" alt="">
+            <p>RES</p>
+          </a>
+        </li>
+        <li class="button-box">
+          <a href="${baseUrl}/cerdo.html">
+            <img src="./public/images/cerdo.png" alt="">
+            <p>CERDO</p>
+          </a>
+        </li>
+        <li class="button-box">
+          <a href="${baseUrl}/centro.html">
+            <img src="./public/images/screen.png" alt="">
+            <p>CENTRO</p>
+          </a>
+        </li>
+        <li class="button-box">
+          <a href="${baseUrl}/pollo.html">
+            <img src="./public/images/pollo.png" alt="">
+            <p>POLLO</p>
+          </a>
+        </li>
+        <li class="button-box">
+          <a href="${baseUrl}/marisco.html">
+            <img src="./public/images/pescado.png" alt="">
+            <p>MARISCO</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <section class="box-preview">
+      <img class="img-preview" src="${sucursal === 'parques' ? 'https://t3.ftcdn.net/jpg/05/85/86/44/360_F_585864419_kgIYUcDQ0yiLOCo1aRjeu7kRxndcoitz.jpg' : 'https://wallpapers-clan.com/wp-content/uploads/2023/11/cool-vaporwave-art-desktop-wallpaper-preview.jpg'}" alt="">
+    </section>
+  `;
+}
 
-    if (sucursal == 'parques') {
-      section = `
-      <section class="section-container">
-          <h1>PANTALLAS</h1>
-          <ul class="container-boxes">
-            <li class="button-box"> 
-                <a href="./routes/parques/res.html">
-                    <img src="./public/images/res.png" alt="">
-                    <p>RES</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/parques/cerdo.html">
-                    <img src="./public/images/cerdo.png" alt="">
-                    <p>CERDO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/parques/centro.html">
-                    <img src="./public/images/screen.png" alt="">
-                    <p>CENTRO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/parques/pollo.html">
-                    <img src="./public/images/pollo.png" alt="">
-                    <p>POLLO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/parques/marisco.html">
-                    <img src="./public/images/pescado.png" alt="">
-                    <p>MARISCO</p>
-                </a>
-            </li>
-        </ul>
-        </section>
-        <section class="box-preview">
-          <img class="img-preview" src="https://t3.ftcdn.net/jpg/05/85/86/44/360_F_585864419_kgIYUcDQ0yiLOCo1aRjeu7kRxndcoitz.jpg" alt="">
-        </section>
-      `;
-    }else if (sucursal == 'valle') {
-      section = `
-      <section class="section-container">
-          <h1>PANTALLAS</h1>
-          <ul class="container-boxes">
-            <li class="button-box"> 
-                <a href="./routes/valle/res.html">
-                    <img src="./public/images/res.png" alt="">
-                    <p>RES</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/valle/cerdo.html">
-                    <img src="./public/images/cerdo.png" alt="">
-                    <p>CERDO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/valle/centro.html">
-                    <img src="./public/images/screen.png" alt="">
-                    <p>CENTRO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/valle/pollo.html">
-                    <img src="./public/images/pollo.png" alt="">
-                    <p>POLLO</p>
-                </a>
-            </li>
-            <li class="button-box">
-                <a href="./routes/valle/marisco.html">
-                    <img src="./public/images/pescado.png" alt="">
-                    <p>MARISCO</p>
-                </a>
-            </li>
-        </ul>
-        </section>
-        <section class="box-preview">
-          <img class="img-preview" src="https://wallpapers-clan.com/wp-content/uploads/2023/11/cool-vaporwave-art-desktop-wallpaper-preview.jpg" alt="">
-        </section>
-      `;
-    }
-    return section;
-  };
+function sectionButtons(sucursal) {
+  let baseUrl = sucursal === 'parques' ? './routes/parques' : './routes/valle';
+  return generateButtonSection(sucursal, baseUrl);
+}
